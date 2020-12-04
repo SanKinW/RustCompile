24c2d30dc06fcc003e6762b0bc50fa9bc1803013
SanKinW <18373275@buaa.edu.cn>
2020-12-04 21:41:52 +0800

    'debug3'

--- src/App.java
+++ src/App.java
@@ -4,10 +4,10 @@ import java.util.List;
 public class App {
     //https://SanKinW:45dc826862b18c003d799faf2d133cf920985c41@github.com/SanKinW/Compile.git
     public static void main(String[] args) throws Exception {
-        String file_path = "src/test.txt";
-        //if (args.length < 3) throw new Exception();
-        //InputStream inputStream = new FileInputStream(args[1]);
-        InputStream inputStream = new FileInputStream(file_path);
+        //String file_path = "src/test.txt";
+        if (args.length < 3) throw new Exception();
+        InputStream inputStream = new FileInputStream(args[1]);
+        //InputStream inputStream = new FileInputStream(file_path);
         Tokenizer.processSource(inputStream);
         for (Token token : Tokenizer.getTokenList()) {
             System.out.print(token.getVal()+" ");
@@ -26,13 +26,14 @@ public class App {
         System.out.println("---------------生成二进制");
         OutToBinary binary = new OutToBinary(Analyser.getGlobals(), Analyser.getStartFunction(), Analyser.getFunctionDefs());
 
-        //DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(args[2])));
-        DataOutputStream out = new DataOutputStream(new FileOutputStream(new File("src/out.txt")));
+        DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(args[2])));
+        //DataOutputStream out = new DataOutputStream(new FileOutputStream(new File("src/out.txt")));
         List<Byte> bytes = binary.generate();
         byte[] resultBytes = new byte[bytes.size()];
         for (int i = 0; i < bytes.size(); ++i) {
             resultBytes[i] = bytes.get(i);
         }
-        out.writeBytes(bytes.toString());
+        //out.writeBytes(bytes.toString());
+        out.write(resultBytes);
     }
 }
